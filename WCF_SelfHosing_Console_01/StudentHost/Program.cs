9877abb4c84f9e5c9ad7ef07fa3df55e586ddb73
marek2222 <marekmusial2@o2.pl>
2018-04-24 09:32:50 +0200

    WCF_SelfHosing_Console_01

---
+++ WCF_SelfHosing_Console_01/StudentHost/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace WCF_SelfHosing_Console_Host
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ServiceHost studentServiceHost = null;
+      try
+      {
+        //Base Address for StudentService
+        Uri httpBaseAddress = new Uri("http://localhost:4321/StudentService");
+
+        //Instantiate ServiceHost
+        studentServiceHost = new ServiceHost(typeof(WCF_SelfHosing_Console.StudentService),
+            httpBaseAddress);
+
+        //Add Endpoint to Host
+        studentServiceHost.AddServiceEndpoint(typeof(WCF_SelfHosing_Console.IStudentService),
+                                                new WSHttpBinding(), "");
+
+        //Metadata Exchange
+        ServiceMetadataBehavior serviceBehavior = new ServiceMetadataBehavior();
+        serviceBehavior.HttpGetEnabled = true;
+        studentServiceHost.Description.Behaviors.Add(serviceBehavior);
+
+        //Open
+        studentServiceHost.Open();
+        Console.WriteLine("Service is live now at : {0}", httpBaseAddress);
+        Console.ReadKey();
+      }
+
+      catch (Exception ex)
+      {
+        studentServiceHost = null;
+        Console.WriteLine("There is an issue with StudentService" + ex.Message);
+      }
+    }
+  }
+}
---
+++ WCF_SelfHosing_Console_01/StudentService/StudentService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_SelfHosing_Console
+{
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+  public class StudentService : IStudentService
+  {
+    public string GetStudentInfo(int studentId)
+    {
+      string studentName = string.Empty;
+      switch (studentId)
+      {
+        case 1:
+          {
+            studentName = "Muhammad Ahmad";
+            break;
+          }
+        case 2:
+          {
+            studentName = "Muhammad Hamza";
+            break;
+          }
+        default:
+          {
+            studentName = "No student found";
+            break;
+          }
+      }
+      return studentName;
+    }
+  }
+}
