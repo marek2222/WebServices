c1c6a194d4bb2a8dcaa9c73f1c04a143e4242954
marek2222 <marekmusial2@o2.pl>
2018-05-18 09:11:34 +0200

    KunVenkat_04_WCF_ConAp_MultipleContracts
    Uwaga: Najpierw hostujemy na aplikacji konsolowej CompanyServiceHost. Uruchamienie z poziomy Visual Studio.  Tworzymy aplikację webową CompanyClient w innej instancji VS. I uruchamiamy przez VS.

---
+++ KunVenkat_04_WCF_ConAp_MultipleContracts/CompanyClient/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CompanyClient
+{
+  public partial class WebForm1 : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+      CompanyService.MyCompanyPublicServiceClient client1
+        = new CompanyService.MyCompanyPublicServiceClient(
+          "BasicHttpBinding_IMyCompanyPublicService");
+      Label1.Text = client1.GetPublicInformation();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+      CompanyService.MyCompanyConfidentialServiceClient client2
+        = new CompanyService.MyCompanyConfidentialServiceClient(
+          "NetTcpBinding_IMyCompanyConfidentialService");
+      Label2.Text = client2.GetCofidentialInformation();
+    }
+
+  }
+}
