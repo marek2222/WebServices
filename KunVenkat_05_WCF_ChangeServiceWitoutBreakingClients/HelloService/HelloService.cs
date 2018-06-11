35f0ad821ca0ec763ef2498f112721e0dbfdbfa3
marek2222 <marekmusial2@o2.pl>
2018-06-11 13:01:58 +0200

    KunVenkat_05_WCF_ChangeServiceWitoutBreakingClients
    Uwaga: Najpierw hostujemy na aplikacji konsolowej HelloServiceHost. Uruchamienie z poziomy Visual Studio. Uruchamiamy aplikację webową HelloWebClient w innej instancji VS. I uruchamiamy przez VS.

---
+++ KunVenkat_05_WCF_ChangeServiceWitoutBreakingClients/HelloService/HelloService.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloService
+{
+  public class HelloService : IHelloServiceChange
+  {
+    public string GetMessage(string name)
+    {
+      return "Hello " + name;
+    }
+  }
+}
---
+++ KunVenkat_05_WCF_ChangeServiceWitoutBreakingClients/HelloService/IHelloService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+namespace HelloService
+{
+    [ServiceContract(Namespace = "http://PragimTech.com/ServiceVersion1"
+      , Name = "IHelloService")]
+    public interface IHelloServiceChange
+    {
+        [OperationContract]
+        string GetMessage(string name);
+    }
+}
