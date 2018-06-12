4b48e99eb59af50b5195ea7ef48a7efafb1dff78
marek2222 <marekmusial2@o2.pl>
2018-06-12 14:04:53 +0200

    Pragim_06_WCF_DataContractAndDataMemberSerialization
    Uwaga: WebService w trybie administratora hostujemy na aplikacji konsolowej EmployeeServiceHost. Uruchamienie z poziomy Visual Studio. W odzielnej instancji VS (administrator) uruchamiamy aplikację webową Client. Aplikacja Client jest oddzielnym projektem w katalogu projektu.

---
+++ Pragim_06_WCF_DataContractAndDataMember/EmployeeService/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace EmployeeService
+{
+  //[DataContract(Namespace = "http://pragimtech.com/Employee")]
+  [DataContract]
+  //[Serializable]
+  public class Employee
+  {
+    //[DataMember]
+    //[DataMember(Name="ID", Order = 1)]
+    [DataMember(Order = 1)]
+    public int Id { get; set; }
+
+    [DataMember(Order = 2)]
+    public string Name { get; set; }
+
+    [DataMember(Order = 3)]
+    public string Gender { get; set; }
+
+    [DataMember(Order = 4)]
+    public DateTime DateOfBirth { get; set; }
+    
+  }
+}
