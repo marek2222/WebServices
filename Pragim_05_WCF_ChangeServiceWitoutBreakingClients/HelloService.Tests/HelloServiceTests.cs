6a612ed32659f3e8bde146cc896947dd3033b098
marek2222 <marekmusial2@o2.pl>
2018-06-15 11:52:23 +0200

    KunVenkat_05_WCF_ChangeServiceWitoutBreakingClients UnitTests

---
+++ Pragim_05_WCF_ChangeServiceWitoutBreakingClients/HelloService.Tests/HelloServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloService.Tests
+{
+  [TestClass]
+  public class HelloServiceTests
+  {
+    private HelloService1.HelloServiceClient client = null;
+
+    [TestInitialize]
+    public void TestInit()
+    {
+      //Arrange test
+      client = new HelloService1.HelloServiceClient("BasicHttpBinding_IHelloService");
+    }
+
+    [TestCleanup]
+    public void TestClean()
+    {
+      client = null;
+    }
+
+
+    [TestMethod]
+    public void Test_EqualMessageIsEmpty()
+    {
+      string message = "";
+      string result = client.GetMessage(message);
+      Assert.AreEqual("Hello " + message, result, "Pusty tekst");
+    }
+
+    [TestMethod]
+    public void Test_EqualMessage()
+    {
+      string message = "message";
+      string result = client.GetMessage(message);
+      Assert.AreEqual("Hello " + message, result, "Poprawny tekst");
+    }
+
+  }
+}
